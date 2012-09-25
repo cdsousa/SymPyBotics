b5297ea236d4b3d7837db43ce72fcc3a4c851e9a
Cristovao Duarte Sousa <crisjss@gmail.com>
2012-09-25 16:43:14 +0100

    Added function generation to dynamic.py.

--- sympybotics/dynamic.py
+++ sympybotics/dynamic.py
@@ -55,6 +55,8 @@ class Dyn(object):
           return func( *args, **kwargs )
         return decorated_function
 
+    self.dof = rbt.dof
+
     self.delta = sympy.Matrix( rbt.dynparms(usefricdyn=usefricdyn) )
     self.n_delta =  len( self.delta )
 
@@ -63,14 +65,16 @@ class Dyn(object):
     M_ivs, M = memoize(dynamic_algorithms.gen_massmatrix_rne)( True, rbt )
     c_ivs, c = memoize(dynamic_algorithms.gen_ccfterm_rne)( True, rbt )
     g_ivs, g = memoize(dynamic_algorithms.gen_gravterm_rne)( True, rbt )
-    f = memoize(dynamic_algorithms.gen_fricterm)( rbt )
+    if usefricdyn:
+      f = memoize(dynamic_algorithms.gen_fricterm)( rbt )
 
     self.tau_code = memoize(codegen.optimize_code)( (tau_ivs, tau.mat), ivarnames='aux' )
     self.regressor_code = memoize(codegen.optimize_code)( (regressor_ivs, regressor.mat), ivarnames='aux' )
     self.M_code = memoize(codegen.optimize_code)( (M_ivs, M.mat), ivarnames='aux' )
     self.c_code = memoize(codegen.optimize_code)( (c_ivs, c.mat), ivarnames='aux' )
     self.g_code = memoize(codegen.optimize_code)( (g_ivs, g.mat), ivarnames='aux' )
-    self.f_code = memoize(codegen.optimize_code)( ([], f.mat), ivarnames='aux' )
+    if usefricdyn:
+      self.f_code = memoize(codegen.optimize_code)( ([], f.mat), ivarnames='aux' )
     
     func_def_regressor = memoize(codegen_robot.dyn_code_to_func)( 'python', self.regressor_code, 'regressor_func', 2, rbt.dof  )
     global sin, cos, sign
@@ -89,4 +93,43 @@ class Dyn(object):
     self.beta = ( self.Pb.T + self.Kd * self.Pd.T ) * self.delta
     self.n_beta = len( self.beta )
 
+    self.gen_member_funcs()
 
+
+  def gen_member_funcs(self):
+    
+    tau_str = codegen_robot.dyn_code_to_func( 'python', self.tau_code, 'tau', 2, self.dof, self.delta )
+    regressor_str = codegen_robot.dyn_code_to_func( 'python', self.regressor_code, 'regressor', 2, self.dof, self.delta )
+    M_str = codegen_robot.dyn_code_to_func( 'python', self.M_code, 'M', 0, self.dof )
+    g_str = codegen_robot.dyn_code_to_func( 'python', self.c_code, 'c', 1, self.dof, self.delta )
+    c_str = codegen_robot.dyn_code_to_func( 'python', self.g_code, 'g', 0, self.dof, self.delta )
+    if hasattr(self, 'f_code'):
+      f_str = codegen_robot.dyn_code_to_func( 'python', self.f_code, 'f', 1, self.dof, self.delta )
+
+    funcs = [ ('tau', self.dof, 1),
+              ('regressor', self.dof, self.n_delta),
+              ('M', self.dof, self.dof),
+              ('c', self.dof, 1),
+              ('g', self.dof, 1)
+            ]
+            
+    if hasattr(self, 'f_code'):
+      funcs.append( ('f', self.dof, 1 ) )
+    
+    for func,m,n in funcs:
+      func_str = eval(func+'_str')
+      func_lines = func_str.splitlines()
+      func_return = func_lines[-1].split()[1]
+      func_lines[-1] = func_lines[-1].replace( func_return, 'sympy.Matrix( ' + str(m) + ', ' + str(n) + ', ' + func_return + ' )' )
+      func_str = '\n'.join(func_lines)
+      compatible_exec( func_str, globals() )
+
+    self.tau = tau
+    self.regressor = regressor
+    self.M = M
+    self.c = c
+    self.g = g
+    if hasattr(self, 'f_code'):
+      self.f = f
+
+    
