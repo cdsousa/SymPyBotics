8dac34241d18c34f3270fdd903f62b3d7de0ec84
Cristovao Duarte Sousa <crisjss@gmail.com>
2013-04-08 12:59:14 +0100

    Apendded 'git' to version number.

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from os.path import exists
 from setuptools import setup
 
 setup(
-name = 'sympybotics',
-version = '0.2',
-author = 'Cristovao D. Sousa',
-author_email = 'crisjss@gmail.com',
-description = 'Robot dynamic symbolic model generator',
-license = 'BSD',
-keywords = 'robot dynamics',
-url = 'http://github.com/cdsousa/sympybotics',
-packages = ['sympybotics'],
-long_description = open('README.md').read() if exists('README.md') else '',
-classifiers = [
+name='sympybotics',
+version='0.2-git',
+author='Cristovao D. Sousa',
+author_email='crisjss@gmail.com',
+description='Robot dynamic symbolic model generator',
+license='BSD',
+keywords='robot dynamics',
+url='http://github.com/cdsousa/sympybotics',
+packages=['sympybotics', 'sympybotics.dynident', 'sympybotics.tools'],
+long_description=open('README.md').read() if exists('README.md') else '',
+classifiers=[
   'Development Status :: 4 - Beta',
   'Intended Audience :: Developers',
   'License :: OSI Approved :: BSD License',
