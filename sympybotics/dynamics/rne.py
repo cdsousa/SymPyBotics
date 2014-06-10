12d81009e323207eb2a7745d0c2180d48e54612a
Cristovao Duarte Sousa <crisjss@gmail.com>
2014-06-10 08:24:50 +0100

    Improve Coriolis matrix generation

--- sympybotics/dynamics/rne.py
+++ sympybotics/dynamics/rne.py
@@ -69,26 +69,42 @@ def coriolismatrix(rbtdef, geom, ifunc=None):
     rbtdeftmp.ddq = zeros((rbtdeftmp.dof, 1))
     rbtdeftmp.frictionmodel = None
 
-    a = {}
+    # # Coriolis vector c element k is given by
+    # # c[k] = dq.T * A[k] * dq[k]
+    # # where A[k] is a (non-unique) matrix
+
+    # # Find upper triangular A[k]'s
+
+    A = [zeros((rbtdef.dof, rbtdef.dof)) for k in range(rbtdef.dof)]
+
     for i in range(rbtdef.dof):
         rbtdeftmp.dq = zeros((rbtdeftmp.dof, 1))
         rbtdeftmp.dq[i] = 1
         geomtmp = Geometry(rbtdeftmp)
         fw_results = rne_forward(rbtdeftmp, geomtmp, ifunc)
-        a[(i, i)] = rne_backward(rbtdeftmp, geomtmp, fw_results, ifunc=ifunc)
+        c = rne_backward(rbtdeftmp, geomtmp, fw_results, ifunc=ifunc)
+        for k in range(rbtdef.dof):
+            A[k][i, i] = c[k]
+
     for i in range(rbtdef.dof):
         for j in range(i+1, rbtdef.dof):
             rbtdeftmp.dq = zeros((rbtdeftmp.dof, 1))
             rbtdeftmp.dq[i] = rbtdeftmp.dq[j] = 1
             geomtmp = Geometry(rbtdeftmp)
             fw_results = rne_forward(rbtdeftmp, geomtmp)
-            a[(i, j)] = rne_backward(rbtdeftmp, geomtmp, fw_results,
-                                     ifunc=ifunc)
-            a[(i, j)] += ifunc(-a[(i, i)] - a[(j, j)])
+            c = rne_backward(rbtdeftmp, geomtmp, fw_results, ifunc=ifunc)
+            for k in range(rbtdef.dof):
+                A[k][i, j] = ifunc(c[k] - A[k][i, i] - A[k][j, j])
 
-    for i in range(rbtdef.dof):
-        for j in range(i, rbtdef.dof):
-            C[:, j] += ifunc(a[(i, j)] * rbtdef.dq[i])
+    # # Coriolis vector c is
+    # # c[k] = dq.T * A[k] * dq[k]
+    # # thus, Coriolis matrix is
+    # # C[k, :] = dq.T * A[k]
+    # # so that
+    # # c[k] = C[k, :] * dq
+
+    for k in range(rbtdef.dof):
+        C[k, :] = rbtdef.dq.T * A[k]
 
     C = ifunc(C)
 
